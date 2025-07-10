58182b07f42c2135f35474086fa23f18ede4786a
D4T4 <149961434+D4T4-DEV@users.noreply.github.com>
2025-07-10 10:43:25 -0600

    Update _layout.tsx
    Actualizamos para poder tener el status bar pintado, actualmente no se hacia

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,35 +1,38 @@
 import React from 'react'
 import { Stack, useRouter } from 'expo-router'
 import { HistoryButton } from '@/shared/ui/molecules/ButtonHistory';
+import { StatusBar } from 'expo-status-bar';
 
 const RootLayout = () => {
     const router = useRouter();
     return (
-
-        <Stack
-            screenOptions={{
-                title: '',
-                headerShadowVisible: false,
-            }}
-        >
-            <Stack.Screen name='index'
-                options={{
+        <>
+            <StatusBar style='dark' />
+            <Stack
+                screenOptions={{
                     title: '',
-                    headerRight: () => (
-                        <HistoryButton
-                            onPress={() => {
-                                router.replace('/history')
-                            }}
-                        />
-                    )
+                    headerShadowVisible: false,
                 }}
-            />
-            <Stack.Screen name='history'
-                options={{
-                    title: ''
-                }}
-            />
-        </Stack>
+            >
+                <Stack.Screen name='index'
+                    options={{
+                        title: '',
+                        headerRight: () => (
+                            <HistoryButton
+                                onPress={() => {
+                                    router.replace('/history')
+                                }}
+                            />
+                        )
+                    }}
+                />
+                <Stack.Screen name='history'
+                    options={{
+                        title: ''
+                    }}
+                />
+            </Stack>
+        </>
     )
 }
 
